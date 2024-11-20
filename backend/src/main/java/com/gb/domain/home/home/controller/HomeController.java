b9e9b07004d25032985360c34d2dc6e96429e559
jhs512 <jangka512@gmail.com>
2024-11-21 00:12:40 +0900

    work

---
+++ backend/src/main/java/com/gb/domain/home/home/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.gb.domain.home.home.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    @ResponseBody
+    public String showMain() {
+        return "안녕하세요.";
+    }
+}
