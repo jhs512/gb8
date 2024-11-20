5097005f3f92fe1630ca7a73bef50bc1951f905f
jhs512 <jangka512@gmail.com>
2024-11-21 01:40:00 +0900

    work

---
+++ backend/src/main/java/com/gb/domain/member/member/service/MemberService.java
@@ -0,0 +1,24 @@
+package com.gb.domain.member.member.service;
+
+import com.gb.domain.member.member.entity.Member;
+import com.gb.domain.member.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public Member join(String username, String password, String nickname) {
+        Member member = Member.builder()
+                .username(username)
+                .password(password)
+                .nickname(nickname)
+                .build();
+
+        return memberRepository.save(member);
+    }
+}
