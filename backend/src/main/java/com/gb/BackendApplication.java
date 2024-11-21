9b8273934f66cc1925d49ecb91a030f65b31c02c
jhs512 <jangka512@gmail.com>
2024-11-22 00:26:07 +0900

    work

--- backend/src/main/java/com/gb/BackendApplication.java
+++ backend/src/main/java/com/gb/BackendApplication.java
@@ -2,8 +2,10 @@ package com.gb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing // @CreatedDate, @LastModifiedDate 를 작동시키기 위한 코드
 public class BackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
--- backend/src/main/java/com/gb/domain/member/member/entity/Member.java
+++ backend/src/main/java/com/gb/domain/member/member/entity/Member.java
@@ -1,10 +1,10 @@
 package com.gb.domain.member.member.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
@@ -14,10 +14,19 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@EntityListeners(AuditingEntityListener.class) // @CreatedDate, @LastModifiedDate 를 작동시키기 위한 코드
 public class Member {
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
+
+    @CreatedDate // INSERT 발생날짜가 자동으로 저장됨
+    @Setter(AccessLevel.NONE) // 외부에서 세터를 사용하지 못하도록
+    private String createDate;
+
+    @LastModifiedDate // UPDATE 발생날짜가 자동으로 저장됨
+    @Setter(AccessLevel.NONE) // 외부에서 세터를 사용하지 못하도록
+    private String modifyDate;
 
     @Column(unique = true)
     private String username;
