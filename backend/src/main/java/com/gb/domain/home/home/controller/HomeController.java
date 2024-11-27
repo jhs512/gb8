221cab544294057be0be4095935009411f6d6448
jhs512 <jangka512@gmail.com>
2024-11-28 00:26:49 +0900

    work

--- backend/src/main/java/com/gb/domain/home/home/controller/HomeController.java
+++ backend/src/main/java/com/gb/domain/home/home/controller/HomeController.java
@@ -2,14 +2,17 @@ package com.gb.domain.home.home.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @Tag(name = "HomeController", description = "Home 컨트롤러")
+@RequestMapping(value = "/", produces = MediaType.TEXT_HTML_VALUE)
 public class HomeController {
-    @GetMapping("/")
+    @GetMapping
     @ResponseBody
     @Operation(summary = "메인")
     public String showMain() {
