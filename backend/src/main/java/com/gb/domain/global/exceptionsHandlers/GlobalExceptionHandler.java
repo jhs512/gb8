b0c91603369a44eaebac44ab5915a35b2ac42dac
jhs512 <jangka512@gmail.com>
2024-11-28 01:16:02 +0900

    work

--- backend/src/main/java/com/gb/domain/global/exceptionsHandlers/GlobalExceptionHandler.java
+++ backend/src/main/java/com/gb/domain/global/exceptionsHandlers/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.gb.domain.global.exceptions.ServiceException;
 import com.gb.global.rsData.RsData;
 import com.gb.standard.base.Empty;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class GlobalExceptionHandler {
     @ExceptionHandler(ServiceException.class)
     public ResponseEntity<RsData<Empty>> handle(ServiceException ex) {
-        return new ResponseEntity<>(ex.getRsData(), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(ex.getRsData(), HttpStatusCode.valueOf(ex.getRsData().getStatusCode()));
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
